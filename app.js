89edcf2c55a09958c901c45514246b8c0e2962d3
mhendricks96 <mhendricks96@yahoo.com>
2020-12-12 23:14:34 -0800

    added questions

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log("Hello, World");
+
+var Whiskey;
+
+Whiskey = "Real Drink!";
+
+console.log(Whiskey);
+
+var age = prompt ("What is your favorite drink");
+
+console.log(age);
+
+var overDrinkingAge = prompt ("how old are you?");
+
+if (Response>20) {
+    console.log ("Excellent!")
+}
+    else {
+        console.log ("Get Out!")
+    }
