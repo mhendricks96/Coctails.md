b7eb3b9a9426dc898633b9e29a3b05b82e25715f
mhendricks96 <mhendricks96@yahoo.com>
2020-12-14 22:49:30 -0800

    figuring out if/else

--- app.js
+++ app.js
@@ -7,13 +7,26 @@ Whiskey = "The only real drink?";
 console.log(Whiskey);
 
 var firstQuestion;
+var alertMessage;
+var firstResponse;
+var overDrinkingAge
 
 firstQuestion = "what is your favorite drink?";
 
 
-var overDrinkingAge = prompt ("how old are you?");
 
-if (Response > 20) {
+firstResponse = prompt (firstQuestion);
+console.log(firstResponse);
+
+if (firstResponse === "old fashion") { 
+    alertMessage = "Excellent choice!"
+ } else {
+    alertMessage = "Gross! grow up and drink an old fashion";
+}
+
+overDrinkingAge = prompt ("how old are you?");
+
+if (overDrinkingAge > 20) {
     console.log ("old enough to advertise")
 }
     else {
